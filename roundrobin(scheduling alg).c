c5826d51f3360b41f8c9124ca60b8123bd29be79
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2025-03-26 17:46:43 +0530

    Update roundrobin(scheduling alg).c

--- roundrobin(scheduling alg).c
+++ roundrobin(scheduling alg).c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-int main() {
+int main() 
+{
     int i, j, n, bt[20], wt[20], tat[20], t, ct[20], max;
     float wtavg = 0, ttavg = 0, temp = 0;
     printf("\nEnter number of processes:\n");
     scanf("%d", &n);
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
+    {
         printf("\nEnter burst time for process %d: ", i + 1);
         scanf("%d", &bt[i]);
         ct[i] = bt[i]; 
@@ -23,16 +25,16 @@ int main() {
         {
             if (bt[i] != 0) 
             {
-                if (bt[i] < t) 
+                if (bt[i] <= t) 
                 {
                     tat[i] = temp + bt[i]; 
-                    temp += bt[i];
+                    temp = temp + bt[i];
                     bt[i] = 0; 
                 } 
                 else 
                 {
-                    bt[i] -= t; 
-                    temp += t;
+                    bt[i] = bt[i] - t; 
+                    temp = temp + t;
                 }
             }
         }
@@ -47,5 +49,23 @@ int main() {
     printf("\nAverage Waiting Time: %.2f", wtavg / n);
     printf("\n\tProcess\tBurst time\tWaiting time\tTurnaround time\n");
     for (i = 0; i < n; i++) 
-        printf("\tP%d\t\t%d\t\t%d\t\t%d\n", i + 1, ct[i], wt[i], tat[i]);
+        printf("\tP%d\t\t\t%d\t\t\t\t%d\t\t\t\t\t%d\n", i + 1, ct[i], wt[i], tat[i]);
 }
+/*output:
+Enter number of processes:
+3
+
+Enter burst time for process 1: 24
+
+Enter burst time for process 2: 3
+
+Enter burst time for process 3: 3
+
+Enter the size of time slice: 3
+
+Average Turnaround Time: 15.00
+Average Waiting Time: 5.00
+	Process	Burst time	Waiting time	Turnaround time
+	P1			24				6					30
+	P2			3				3					6
+	P3			3				6					9*/
